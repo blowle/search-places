87ef852ef1b87fd674ebf8533ec3fbc473a68f8d
yongcheol Lee <yongcheol.dev@gmail.com>
2021-12-24 23:08:00 +0900

    Initial Commit

---
+++ FindPlaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FindPlaces
+//
+//  Created by yongcheol Lee on 2021/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
